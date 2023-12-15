744bd7f4fddc16bf20a72bf4106e7d2ac222b16a
Farouk Zakaria <123017280+FaroukZakaria@users.noreply.github.com>
2023-12-16 00:06:30 +0200

    Simple todo list (structuring in progress)

---
+++ Todo_list/main.py
@@ -0,0 +1,21 @@
+add = __import__('todo_list').todo_add
+complete = __import__('todo_list').todo_complete
+view = __import__('todo_list').todo_view
+remove = __import__('todo_list').todo_remove
+
+print("(h for help)")
+while True:
+    interpreter = str(input("Select your action: "))
+    
+    if interpreter == 'h':
+        print("""1 for adding a task\n
+              2 for completing a task\n
+              3 for removing a task\n
+              4 for viewing a task\n
+              """)
+        continue
+    
+    elif interpreter == '1':
+        title = input("Enter the title of task: ")
+        due_date = input("Enter the due date (ignore for none): ")
+        add(title, due_date=None)
---
+++ Todo_list/todo_list.py
@@ -0,0 +1,12 @@
+def todo_add():
+    pass
+
+def todo_complete():
+    pass
+
+def todo_view():
+    pass
+
+def todo_remove():
+    pass
+
