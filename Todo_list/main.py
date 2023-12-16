719ccaddead719d0205a8f31ab2e01e224595806
Farouk Zakaria <123017280+FaroukZakaria@users.noreply.github.com>
2023-12-16 22:53:53 +0200

    Todo list layout

--- Todo_list/main.py
+++ Todo_list/main.py
@@ -1,11 +1,11 @@
-add = __import__('todo_list').todo_add
+"""import todo_list
 complete = __import__('todo_list').todo_complete
 view = __import__('todo_list').todo_view
-remove = __import__('todo_list').todo_remove
+remove = __import__('todo_list').todo_remove"""
 
 print("(h for help)")
 while True:
-    interpreter = str(input("Select your action: "))
+    interpreter = (str(input("Select your action: ")))
     
     if interpreter == 'h':
         print("""1 for adding a task\n
@@ -14,8 +14,33 @@ while True:
               4 for viewing a task\n
               """)
         continue
+
+    elif (interpreter == 'exit' or
+    interpreter == 'quit' or
+    interpreter == 'q' or
+    KeyboardInterrupt):
+        print("Bye")
+        break
+
     
     elif interpreter == '1':
         title = input("Enter the title of task: ")
         due_date = input("Enter the due date (ignore for none): ")
-        add(title, due_date=None)
+        # add(title, due_date=None)
+    
+    elif interpreter == '2':
+        #view()
+        task = input("Select task: ")
+        #complete(task)
+    
+    elif interpreter == '3':
+        #view()
+        task = input("Select task: ")
+        #remove(task)
+    
+    elif interpreter == '4':
+        #view()
+        pass
+    
+    else:
+        print ("I'm sorry. Invalid answer, try again.")
